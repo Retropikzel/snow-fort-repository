c714ae154bdce480e92caee49997be039f3b03f9
retropikzel <retropikzel@iki.fi>
2026-08-28 11:14:20 +0300

    Create snow repo

---
+++ .snow-repo.scm
@@ -0,0 +1,13 @@
+(repository
+  (name "snow-fort-github")
+  (url "https://github.com/Snow-fort-repository/snow-fort-repository.git")
+  (sibling
+    (name "snow-fort-original")
+    (url "http://snow-fort.org/s/repo.scm"))
+  (sibling
+    (name "snow-fort-gitlab")
+    (git (url "https://gitlab.com/snow-fort-repository/snow-fort-repository.git")))
+  (sibling
+    (name "snow-fort-codeberg")
+    (git (url "https://codeberg.org/Snow-fort-repository/snow-fort-repository.git"))))
+
